b79cc8ccc2667dc4de7204b1fbd0b7cabed563a1
MaelleH <h.maelle.98@gmail.com>
2019-01-15 13:45:35 +0100

    CaCompile

--- Projet/src/project_v2.c
+++ Projet/src/project_v2.c
@@ -80,7 +80,7 @@ void projectV2(const char * i_file, const char * o_file, unsigned long nb_split)
 
   /* 3 - Merge (two by two) */
   pid_t pidMerge = fork();
-  /*if (pidMerge == -1){
+  if (pidMerge == -1){
     perror("Fail merge error");
   }else if(pidMerge == 0){
     unsigned long h = ((nb_split-nb_split%2)/2);
@@ -88,7 +88,7 @@ void projectV2(const char * i_file, const char * o_file, unsigned long nb_split)
     exit(0);
   }else{
     projectV2_combMerge(((nb_split-nb_split%2)/2+1),nb_split, (const char **) filenames_sort, (const char *) o_file);
-    wait(pidMerge);
+    wait(&pidMerge);
     projectV2_combMerge(((nb_split-nb_split%2)/2+1),nb_split, (const char **) filenames_sort, (const char *) o_file);
   }
 
